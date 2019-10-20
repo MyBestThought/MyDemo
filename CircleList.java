0f80aeb81e13313c649e230b3a9354310ff99b05
YHT97 <1546558782@qq.com>
2019-10-20 10:38:10 +0800

    判断链表是否带环和获取环形链表的入口点

--- CircleList.java
+++ CircleList.java
@@ -35,4 +35,28 @@ public class CircleList {
         }
         return false;
     }
+	//环形链表的入口点---双指针
+	public ListNode detectCycle(ListNode head) {
+        boolean flag = false;
+        ListNode slow = head, fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
+                flag = true;
+                break;
+            }
+        }
+        if(flag == true){
+            fast = head;
+            while(fast != slow){
+                fast = fast.next;
+                slow = slow.next;
+            }
+            return fast;
+        }else{
+            return null;
+        }
+        
+    }
 }
