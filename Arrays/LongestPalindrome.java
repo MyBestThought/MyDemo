c01dd39272883ffc073c4cf65fbcccf1813ad0f3
YHT97 <1546558782@qq.com>
2020-01-18 17:55:39 +0800

    暴力破解法和动态规划实现最长回文子串

---
+++ Arrays/LongestPalindrome.java
@@ -0,0 +1,75 @@
+public class LongestPalindrome {
+
+	//方法一：暴力破解法
+	public String longestPalindrome1(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+        int maxLen = 1;
+        String res = s.substring(0, 1);
+        // 枚举所有长度大于等于 2 的子串
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (j - i + 1 > maxLen && valid(s, i, j)) {
+                    maxLen = j - i + 1;
+                    res = s.substring(i, j + 1);
+                }
+            }
+        }
+        return res;
+    }
+    private boolean valid(String s, int left, int right) {
+        // 验证子串 s[left, right] 是否为回文串
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+	//方法二：动态规划
+    public String longestPalindrome2(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        boolean[][] dp = new boolean[len][len];
+
+        // 初始化
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        int maxLen = 1;
+        int start = 0;
+
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                } else {
+                    dp[i][j] = false;
+                }
+
+                // 只要 dp[i][j] == true 成立，就表示子串 s[i, j] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j]) {
+                    int curLen = j - i + 1;
+                    if (curLen > maxLen) {
+                        maxLen = curLen;
+                        start = i;
+                    }
+                }
+            }
+        }
+        return s.substring(start, start + maxLen);
+    }
+}
